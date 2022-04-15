81219bd33b8ba67d7547b9272425bf976f32d801
Kidong Yun <oriondigestive@gmail.com>
2022-04-15 14:01:49 +0900

    change inner architecture, implements service layer

---
+++ sheet-service/src/main/kotlin/com/kian/yun/sheetshow/sheet/service/SheetServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.kian.yun.sheetshow.sheet.service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SheetServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<SheetServiceApplication>(*args)
+}
---
+++ sheet-service/src/test/kotlin/com/kian/yun/sheetshow/sheet/service/SheetServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.kian.yun.sheetshow.sheet.service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SheetServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
